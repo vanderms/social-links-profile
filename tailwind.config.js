94037222ab8c61cf761bd9f48b7f0d14a404c02b
Vander Santos <vanderms.84@gmail.com>
2024-04-01 22:19:48 -0300

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          "000": "var(--neutral-000)",
+          700: "var(--neutral-700)",
+          900: "var(--neutral-900)",
+          999: "var((--neutral-999)",
+        },
+        primary: {
+          700: "var(--primary-700)",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
